707b28875a3850990d9d9619687e34e4a46a5dbd
Githubuser1708 <141693329+Githubuser1708@users.noreply.github.com>
2023-08-14 23:41:37 +0800

    Add files via upload

---
+++ For_loop_exercise.py
@@ -0,0 +1,14 @@
+# Exercise: 7-Up
+
+# Step 1: generate a list of numbers from 1 to 100
+numlist = list(range(1, 101))
+print(numlist)
+
+# Step 2: loop through this list using for loop
+for i in range(len(numlist)):
+
+    # Step 3: if the element is a multiple of 7 or contains the digit 7, replace with '7-Up'
+    if numlist[i] % 7 == 0 or "7" in str(numlist[i]):
+        numlist[i] = '7-Up!'
+
+print(numlist)
